069bb006030d0502cdaca174331e52224c2ca5fc
ivansanchezvera <ivansanchez_1988@hotmail.com>
2017-06-26 22:11:51 -0500

    Bug Fixed: Arreglado problema con el media player, para poder ver videos desde url en internet y videos desde archivos locales de la PC

--- src/controllers/ControllerCooperativa.java
+++ src/controllers/ControllerCooperativa.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -52,10 +54,25 @@ public class ControllerCooperativa {
 	
 	
 	private void cargarMedia() {
-		Media media = new Media("http://anime4.streamregarder.com/videoplayback?hash=466775c85213aa3ec85991d296a5ee1656834808db382cc61920963ed21a9da1d77dea57f02e1c470e936bfd7bf9bddc4f57c4e0cd2ac402ea59f59e9a3a6dbd831e12bcf69335968348d222e45d58b54220a35e6bf12a1724969431c5c31d807d682de55d9f8f00c36cbd768445acaf5065f3c8fd1f76dc5964d5f19e766bf05dcc0187e99a5ddf1faa532b3d51526e3189b7f963848f58abaaab23daac64cdab1b72a8ce3e4850d3326dbe2b853afea1686922b0381be2264b1bde4fd1cf2fffcb14bf407f9d61a4fe388b74d0dc1124f22b6d083859080e07f013f0be2b61746cedd786200785ac14eeb4ed2f5158a0bf3713e96f7471de799a793c47cf8a3508cca3a95258e4172d8e1ccfb45b0da963df24e8a051dabbb902106cc3d000866d1cd759081044f0742725742191a46b2bbe833bead85fcd27875a44bf35d8e9092295618b38a2dea245093ae49f65001218c77e82a6b09bec633c0c41dacac7a63ccad528d30545df8460cb0f372f2dc47d46847b11ccef91b9199a6c54b2247566e418fe93d0ad6ac75b6a54918652bc4230cfc0d1bf34bc1eaa1a49376fe71a0a&id=94d74898298ff17c&itag=18&source=webdrive&requiressl=yes&ttl=transient&mm=30&mn=sn-5uaezn6r&ms=nxu&mv=m&pl=20&ei=lhBMWYD6J5TMqwXCjJX4Bg&mime=video/mp4&lmt=1471728566159177&mt=1498157087&ipbits=0&expire=1498171606&cp=QVJOWkJfUFZOR1hNOmMxeFJwY0dWVkhz&sparams=ip,ipbits,expire,id,itag,source,requiressl,ttl,mm,mn,ms,mv,pl,ei,driveid,mime,lmt,cp&signature=60D20AA0CF0F22E1478FA8CEBB8C946B5973EA76.B164B99F0BE7CE329B11B695CEB413FA5C4FEC29");
-		MediaPlayer mediaPlayer = new MediaPlayer(media);
-		mediaPlayer.setAutoPlay(true);
-		mediaCooperativa.setMediaPlayer(mediaPlayer);
+		try{
+
+			//Path to Online Video Resource
+			String pathName = "http://clips.vorwaerts-gmbh.de/VfE_html5.mp4";
+			URI u = URI.create(pathName);
+
+			//Path to File
+			//String pathName = "H:\\Downloads\\SwarmThousandRobots.mp4";
+			//URI u = new File(pathName).toURI();
+
+			Media media = new Media(u.toString());
+			MediaPlayer mediaPlayer = new MediaPlayer(media);
+			mediaPlayer.setAutoPlay(true);
+			mediaCooperativa.setMediaPlayer(mediaPlayer);
+			mediaPlayer.play();
+		}catch (Exception e) {
+			System.err.println("Error en el media view" + e);
+			e.printStackTrace();
+		}
 	}
 
 
